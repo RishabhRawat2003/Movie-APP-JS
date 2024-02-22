88bcc580204616f4b302e2c67ccb1b5c410ca67a
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-02-23 00:26:23 +0530

    Auto slider works mostly done

--- main/mainWindow.js
+++ main/mainWindow.js
@@ -1,22 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const paramValue = urlParams.get('param');
 
-
-async function topBoxOffice() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'daf59ec7bamsh1a62797a8782ef7p12ae99jsna6c0fc4a8c54',
-            'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com'
-        }
-    };
-
-    const response = await fetch('https://moviesverse1.p.rapidapi.com/top-box-office', options)
+async function allShows() {
+    const response = await fetch('https://api.tvmaze.com/shows')
     const data = await response.json()
     return data
 }
 
-const topMovie = topBoxOffice()
+const shows = allShows()
+
+
 
 
 //Header Functionality starts
@@ -103,3 +96,65 @@ if (paramValue === 'Guest') {
 }
 
 //Header Functionality Ends
+
+
+//Body Functionality Starts
+
+const slides = document.querySelector('.slides')
+
+shows.then((val) => {
+    const movieDetails = val.slice(10, 20)
+    movieDetails.map((items) => {
+        const images = items.image.original
+        const id = items.id
+        let genres = items.genres //array of generes
+        const name = items.name
+        const year = items.premiered
+        const rating = items.rating.average
+        const summary = items.summary
+        console.log(items)
+        silderImages(images,name,genres,year,summary)
+    })
+})
+
+function silderImages(img,nameOfTheShow ,generes, year , summary) {
+    let anchor = document.createElement('a')
+    let image = document.createElement('img')
+    let name = document.createElement('span')
+    let yearGenres = document.createElement('p')
+    let summaryText = document.createElement('span')
+    let watchTrailer = document.createElement('span')
+    let watchlist = document.createElement('span')
+    yearGenres.setAttribute('class','absolute bottom-52 mx-5 w-full z-10 text-gray-400 font-bold text-sm font-newFont')
+    summaryText.setAttribute('class','absolute mt-2 bottom-16 mx-5 w-80 z-10 text-white font-bold text-xs font-newFont')
+    image.setAttribute('class', 'min-h-full min-w-full object-cover opacity-40 overflow-hidden')
+    anchor.setAttribute('class', 'min-h-full min-w-full overflow-hidden')
+    name.setAttribute('class','absolute bottom-60 mx-5 w-full z-10 text-white font-bold text-4xl font-newFont')
+    watchTrailer.setAttribute('class','absolute bottom-0 mx-5 w-40 z-10 text-white font-bold text-lg font-newFont')
+    watchlist.setAttribute('class','absolute top-0 mx-5 w-40 z-10 text-white font-bold text-lg font-newFont')
+    summaryText.innerHTML += `${summary.slice(0,400)} <a class="select-none text-blue-500 cursor-pointer active:underline active:text-blue-700">See More</a>` 
+    yearGenres.innerHTML += `${year.slice(0,4)} • ${generes}`
+    watchTrailer.innerHTML = 'Watch trailer'
+    watchlist.innerHTML = 'Add Watchlist'
+    name.innerHTML = nameOfTheShow
+    image.src = img
+    anchor.appendChild(name)
+    anchor.appendChild(yearGenres)
+    anchor.appendChild(summaryText)
+    anchor.appendChild(watchTrailer)
+    anchor.appendChild(watchlist)
+    anchor.appendChild(image)
+    slides.appendChild(anchor)
+}
+
+/*
+setInterval(() => {
+    let a = slides.firstElementChild.offsetWidth * 9
+    if(slides.scrollLeft >= a){
+        slides.scrollLeft = 0
+    }else{
+        slides.scrollLeft += slides.firstElementChild.offsetWidth
+    }
+    console.log(slides.scrollLeft);
+}, 7000);
+*/
